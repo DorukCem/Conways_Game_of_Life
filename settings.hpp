b7fe3faf59900b733d6911103ded411404eb0a65
DorukCem <114157471+DorukCem@users.noreply.github.com>
2023-02-21 12:58:01 +0300

    Delete settings.hpp

--- settings.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr int WIDTH = 1200;
-constexpr int HEIGHT = 800;
-constexpr int TILE_SIZE = 50;
-constexpr int NUM_HORIZONTAL_TILES = WIDTH/TILE_SIZE;
-constexpr int NUM_VERTICAL_TILES = HEIGHT/TILE_SIZE;
